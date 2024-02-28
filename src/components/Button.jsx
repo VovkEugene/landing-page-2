448ef792f322b8a0ba5d5a49e45fa65e1508a559
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-28 23:59:12 +0300

    make CardDeal component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`${styles} py-4 px-6 outline-none font-poppins font-medium text-[18px] text-primary bg-blue-gradient`}
+    className={`${styles} py-4 px-6 rounded-[10px] outline-none font-poppins font-medium text-[18px] text-primary bg-blue-gradient`}
   >
     Get Started
   </button>
