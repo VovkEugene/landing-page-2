1e309685845cf334393505321c44bd8026504348
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-28 22:03:29 +0300

    make Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,12 +17,33 @@ const Hero = () => (
       <div className="w-full  flex flex-row justify-between items-center">
         <h1 className="flex-1 font-poppins font-semibold ss:leading-[100px] leading-[75px] ss:text-[72px] text-[52px] text-white">
           The Next <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span> Payment Method.
+          <span className="text-gradient">Generation</span>
         </h1>
         <div className="md:mr-4 mr-0 ss:flex hidden">
           <GetStarted />
         </div>
       </div>
+      <h1 className="w-full font-poppins font-semibold ss:leading-[100px] leading-[75px] ss:text-[68px] text-[52px] text-white">
+        Payment Method.
+      </h1>
+      <p className={`max-w-[470px] mt-5 ${styles.paragraph}`}>
+        Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual
+        fees.
+      </p>
+    </div>
+    <div className={`relative md:my-0 my-10 flex flex-1 ${styles.flexCenter}`}>
+      <img
+        src={robot}
+        alt="billing"
+        className="relative w-[100%] h-[100%] z-[5]"
+      />
+      <div className="absolute w-[40%] h-[35%] top-0 pink__gradient z-[0]" />
+      <div className="absolute w-[80%] h-[80%] bottom-40 rounded-full white__gradient z-[0]" />
+      <div className="absolute w-[50%] h-[50%] right-20 bottom-20 blue__gradient z-[0]" />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
     </div>
   </section>
 );
