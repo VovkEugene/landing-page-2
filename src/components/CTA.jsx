03538a629835a6dec8b038024297c85feb2974dd
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-01 15:29:17 +0300

    make CTA (call-to-active) section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section
+    className={`${styles.marginY} ${styles.padding} ${styles.flexCenter} sm:flex-row flex-col rounded-[20px] box-shadow bg-black-gradient-2`}
+  >
+    <div className="flex flex-col flex-1">
+      <h2 className={styles.heading2}>Let’s try our service now!</h2>
+      <p className={`max-w-[470px] mt-5 ${styles.paragraph}`}>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+    <div className={`sm:ml-10 ml-0 sm:mt-0 mt-10 ${styles.flexCenter}`}>
+      <Button />
+    </div>
+  </section>
+);
 
-export default CTA
+export default CTA;
